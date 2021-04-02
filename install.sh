2fe8425183080598194d511158b48217adea2579
zhaomanzhou <zmzsstreet@163.com>
2021-04-02 21:30:29 +0800

    初始提交

--- install.sh
+++ install.sh
@@ -240,7 +240,7 @@ install_basic() {
   apt-get update
   # 安装必要软件
 
-  if ! apt install vim net-tools wget unzip nginx -y; ; then
+  if ! apt install vim net-tools wget unzip nginx -y; then
     error "安装vim net-tools wget unzip nginx失败"
     exit 1
   fi
@@ -259,40 +259,52 @@ menu() {
     echo "监测到主机域名为$this_server_name"
   fi
 
-  echo "1.一键安装全部"
-  echo "2.安装nginx   http验证tls"
-  echo "3.安装nginx   dns验证tls"
-  echo "4.安装v2ray和中间件"
-  echo "0.退出"
-  read -p ">:" choice
-  case $choice in
-	1)
-		install_basic
-		nginx_conf_simple
-		auto_install_https
-		install_vmanager
-		install_service
-		;;
-  2)
-    install_basic
-		nginx_conf_simple
-		auto_install_https
-    ;;
-  3)
-    nginx_conf_simple
 
-    ;;
-	4)
-	  install_vmanager
-		install_service
-		uninstall
-		;;
-  0)
-    exit 0;
-	*)
-		error
-		;;
-	esac
+  for ((;;));do
+          echo "1.一键安装全部"
+          echo "2.安装nginx   http验证tls"
+          echo "3.安装nginx   dns验证tls"
+          echo "4.安装v2ray和中间件"
+          echo "0.退出"
+          read -p ">:" choice
+          case $choice in
+          1)
+            if ! install_basic ; then
+              return ;
+            fi
+
+
+            if ! nginx_conf_simple; then
+              return;
+            fi
+
+            auto_install_https
+            install_vmanager
+            install_service
+            ;;
+          2)
+            install_basic
+            nginx_conf_simple
+            auto_install_https
+            ;;
+          3)
+            nginx_conf_simple
+
+            ;;
+          4)
+            install_vmanager
+            install_service
+            uninstall
+            ;;
+          0)
+            exit 0;
+            ;;
+          *)
+            error
+            ;;
+          esac
+  done
+
 }
 
 menu
